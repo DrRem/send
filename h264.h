b82dee7dada3fd233dbd97c8a350101749c05e20
DrRem <fffeifei151@gmail.com>
2022-03-20 23:08:29 +0800

    commit

---
+++ h264.h
@@ -0,0 +1,136 @@
+//
+// Created by Administrator on 2022/3/7.
+//
+
+#ifndef SEND_H264_H
+#define SEND_H264_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <netdb.h>
+#include <linux/string.h>
+#include <linux/unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PACKET_BUFFER_END      (unsigned int)0x00000000
+#define MAX_RTP_PKT_LENGTH     1400
+#define DEST_IP                "192.168.240.129" /* 显示端 IP 地址 */
+#define DEST_PORT              12346
+#define DEST_PORT1              12348
+#define H264                   96
+#define PRTP                   101
+
+#define DELTA                  10000          //us
+#define Measure_message_length 600         //bytes
+#define File_location          "/tmp/tmp.mtUGeb20dO/test.h264"
+#define Timestamp_parameter    1640966400000000
+
+typedef int SOCKET;
+
+/******************************************************************
+RTP_FIXED_HEADER
+0               1               2               3
+0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|V=2|P|X|  CC   |M|     PT      |       sequence number         |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|                           timestamp                           |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|           synchronization source (SSRC) identifier            |
++=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+|            contributing source (CSRC) identifiers             |
+|                             ....                              |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+******************************************************************/
+typedef struct
+{
+    /* byte 0 */
+    unsigned char csrc_len:4;       /* expect 0 */
+    unsigned char extension:1;      /* expect 1, see RTP_OP below */
+    unsigned char padding:1;        /* expect 0 */
+    unsigned char version:2;        /* expect 2 */
+    /* byte 1 */
+    unsigned char payload:7;        /* RTP_PAYLOAD_RTSP */
+    unsigned char marker:1;         /* expect 1 */
+    /* bytes 2, 3 */
+    unsigned short seq_no;
+    /* bytes 4-7 */
+    unsigned  int timestamp;      /* !!Can not use long type,long = 8 Byte int 64 bit  system!!*/
+    /* bytes 8-11 */
+    unsigned int ssrc;            /* stream number is used here. */
+} RTP_FIXED_HEADER;
+
+
+
+
+/******************************************************************
+NALU_HEADER
++---------------+
+|0|1|2|3|4|5|6|7|
++-+-+-+-+-+-+-+-+
+|F|NRI|  Type   |
++---------------+
+******************************************************************/
+typedef struct {
+    //byte 0
+    unsigned char TYPE:5;
+    unsigned char NRI:2;
+    unsigned char F:1;
+
+} NALU_HEADER;/* 1 BYTES */
+
+
+
+/******************************************************************
+FU_INDICATOR
++---------------+
+|0|1|2|3|4|5|6|7|
++-+-+-+-+-+-+-+-+
+|F|NRI|  Type   |
++---------------+
+******************************************************************/
+typedef struct {
+    //byte 0
+    unsigned char TYPE:5;
+    unsigned char NRI:2;
+    unsigned char F:1;
+} FU_INDICATOR; /* 1 BYTES */
+
+
+
+/******************************************************************
+FU_HEADER
++---------------+
+|0|1|2|3|4|5|6|7|
++-+-+-+-+-+-+-+-+
+|S|E|R|  Type   |
++---------------+
+******************************************************************/
+typedef struct {
+    //byte 0
+    unsigned char TYPE:5;
+    unsigned char R:1;
+    unsigned char E:1;
+    unsigned char S:1;
+} FU_HEADER;	/* 1 BYTES */
+
+
+
+typedef struct
+{
+    /* bytes 0-3 */
+    unsigned int send_Index;
+    /* bytes 4-7 */
+    unsigned int send_count;
+    /* bytes 8-11 */
+    unsigned int send_time;
+
+}PRTPDATA;
+
+#endif //SEND_H264_H
